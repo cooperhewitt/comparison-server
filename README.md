9768f53c7bf07d768c679d0556bcbe0550259273
Micah Walter <micah@micahwalter.com>
2013-09-09 15:06:00 -0400

    added most of the code from attilaolah

--- README.md
+++ README.md
@@ -3,6 +3,23 @@ comparison-server
 
 takes two images and returns a set of comparison metrics
 
+    $ curl -F image_a=@/path/to/image_a.jpg -F image_b=@/path/to/image_b.jpg http//0.0.0.0:5000/
+	
+
+resulting in something like this
+
+    {
+	  "metrics": {
+	    "levenshtein": 24.479166666666657, 
+	    "nrmsd": 98.73465815317296, 
+	    "psnr": 37.955842578546836, 
+	    "similarity": 61.60691240991981
+	  }, 
+	  "stat": "ok"
+	}
+	
+
+
 dependancies
 ---
 
--- comparison.py
+++ comparison.py
@@ -1,8 +1,39 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify, request
+import Image
+
+import pprint
+
+from lib.imgcmp import *
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-	return "comparison server"
+	
+	image_a = request.files['image_a']
+	image_b = request.files['image_b']
+			
+	img_a = Image.open(image_a)
+	img_b = Image.open(image_b)
+	
+	metrics = compare(img_a, img_b)
+
+	return jsonify(stat="ok", metrics=metrics)
+	
+	
+def compare(image_a, image_b):
+	
+	# stubin out here
+		
+	cmp = FuzzyImageCompare(image_a, image_b)
+	sim = cmp.similarity()
+	
+	stats = cmp.compare()
+	
+	metrics = { "levenshtein": stats['levenshtein'] , 
+				"nrmsd":stats['nrmsd'], 
+				"psnr":stats['psnr'], 
+				"similarity":sim  }
+	
+	return metrics
